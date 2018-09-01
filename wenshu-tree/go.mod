56cc70ce2b9d465fa53408fb09d5465381c6bce6
hearts <hearts.zhang@gmail.com>
2018-09-01 23:46:48 +0800

    - FEATURE demo of code inspector

---
+++ wenshu-tree/go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/hearts.zhang/wenshu-tree
+
+require github.com/otiai10/gosseract v2.2.0+incompatible
--- wenshu-tree/wenshu-tree.go
+++ wenshu-tree/wenshu-tree.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
+import (
+	"fmt"
 
+	"github.com/otiai10/gosseract"
+)
+
+func main() {
+	client := gosseract.NewClient()
+	defer client.Close()
+	client.SetImage("./ValidateCode.jpeg")
+	text, err := client.Text()
+	fmt.Println(text, err)
 }
