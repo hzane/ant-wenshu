c0a5242059cfff170e09d8111d40b1d41ef71171
hunks <hearts.zhang@gmail.com>
2018-09-17 21:17:14 +0800

    - TEMPED 临时增加一个OCR

--- wenshu/misc.go
+++ wenshu/misc.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/robertkrimen/otto"
+	"gitlab.com/hearts.zhang/tools"
 )
 
 func verb(method, uri string, resp *http.Response, err error) {
@@ -18,3 +32,173 @@ func verb(method, uri string, resp *http.Response, err error) {
 func randSleep() {
 	time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
 }
+
+// SubmitForm ...
+func SubmitForm(client *tools.Client, uri string, body url.Values) (*http.Response, error) {
+	req, _ := http.NewRequest("POST", uri, bytes.NewBufferString(body.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+
+	resp, err := client.Do(req)
+	verb("POST", uri, resp, err)
+	return resp, err
+}
+
+type opt = func(url.Values)
+
+func set(k, v string) opt {
+	return func(body url.Values) {
+		body.Set(k, v)
+	}
+}
+func Submit(client *tools.Client, uri string, params ...opt) (*http.Response, error) {
+	body := url.Values{}
+	for _, set := range params {
+		set(body)
+	}
+	return SubmitForm(client, uri, body)
+}
+
+// JSONStringBody ...
+func JSONStringBody(r io.Reader) (ret string, err error) {
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+	vm := otto.New()
+	ret, err = vmRunS(vm, string(body))
+	if err != nil {
+		return
+	}
+	ret, err = vmRunS(vm, fmt.Sprintf(`JSON.stringify(%s)`, ret)) // -> json string
+	return
+}
+
+// VL5X ...
+func VL5X(client *tools.Client) string {
+	vjkl5 := GetVJKL5FromCookie(client)
+	ret, _ := vl5x(vjkl5)
+	return ret
+}
+
+// GetVJKL5FromCookie ...
+func GetVJKL5FromCookie(client *tools.Client) string {
+	uri, _ := url.Parse(host)
+	cookies := client.Jar.Cookies(uri)
+	for _, ck := range cookies {
+		if ck.Name == "vjkl5" {
+			return ck.Value
+		}
+	}
+	return ""
+}
+
+func pretty(jo interface{}) string {
+	b, _ := json.MarshalIndent(jo, "", "  ")
+	return string(b)
+}
+
+func prettys(js string) string {
+	var ret bytes.Buffer
+	_ = json.Indent(&ret, []byte(js), "", "  ")
+	return ret.String()
+}
+
+// AESKey ...
+// https://www.jianshu.com/p/1dc99e3d927c
+func AESKey(runeval string) (key string, err error) {
+	vm := otto.New()
+	compile(vm, config.js, "docid.js")
+	js, err := vm.Run(fmt.Sprintf(`GetJs("%v")`, runeval))
+	if err != nil {
+		return
+	}
+	jss, err := js.ToString()
+	if err != nil {
+		return
+	}
+	statements := strings.Split(jss, ";;")
+	statements[0] = statements[0] + ";" // $hidescript=...
+	js, err = vm.Run(statements[0])     // Tm('._KEY="6942871305;,*Mh)
+	if err != nil {
+		return
+	}
+	jss, err = js.ToString()
+	if err != nil {
+		return
+	}
+	log.Println(jss)
+
+	r := regexp.MustCompile(`_\[_\]\[_\]\((.*?)\)\(\);`)
+	xs := r.FindStringSubmatch(statements[1])[1]
+	xs = strings.Replace(xs, "$hidescript", strconv.Quote(jss), -1)
+	js, err = vm.Run(xs)
+	if err != nil {
+		return
+	}
+	jss, err = js.ToString()
+	if err != nil {
+		return
+	}
+	// setTimeout('com.str._KEY="a69e42871c4f499c930c755edbf6d7d1";',8000*Math.random());
+	r = regexp.MustCompile(`_KEY="(.*)";'`)
+	key = r.FindStringSubmatch(jss)[1]
+	return
+}
+
+// GetCode ...
+func GetCode(client *tools.Client, guid string) (number string) {
+	data := url.Values{}
+	data.Set("guid", guid)
+
+	req, _ := http.NewRequest("POST", GetCodeURL, bytes.NewBufferString(data.Encode()))
+	req.Header.Set("Origin", host)
+	req.Header.Set("Referer", host)
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	b, _ := ioutil.ReadAll(resp.Body)
+
+	number = string(b)
+	return
+}
+
+// GUID ...
+func GUID() string {
+	uuid := make([]byte, 16)
+	rand.Read(uuid)
+
+	return fmt.Sprintf("%x-%x-%x%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
+}
+
+func vmRunS(vm *otto.Otto, src string) (string, error) {
+	val, err := vm.Run(src)
+	if err == nil {
+		return val.ToString()
+	}
+	return "", err
+}
+
+func vl5x(vjkl5 string) (string, error) {
+	vm := otto.New()
+	compile(vm, path.Join(config.js, "vl5x.js"))
+
+	value, err := vm.Run(fmt.Sprintf(`GetVl5x("%v")`, vjkl5))
+	if err != nil {
+		return "", err
+	}
+	return value.ToString()
+}
+func compile(vm *otto.Otto, files ...string) {
+	for _, file := range files {
+		if bjs, err := ioutil.ReadFile(file); err == nil {
+			vm.Run(string(bjs))
+		}
+	}
+}
+
--- wenshu/wenshu.go
+++ wenshu/wenshu.go
@@ -14,7 +14,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -46,7 +45,7 @@ func main() {
 	if config.params != "" {
 		_, cases, _ := ListContent(client, number, guid, 1, 5, config.params)
 		for _, cise := range cases {
-			//fmt.Println(pretty(cise))
+			// fmt.Println(pretty(cise))
 			json.NewEncoder(os.Stdout).Encode(cise)
 		}
 	}
@@ -106,9 +105,9 @@ func mainxii() {
 		params := params + ",裁判年份:" + year.key
 		for _, typi := range items["文书类型"] {
 			params := params + ",文书类型:" + typi.key
-			//ids, err := ListContent(client, number, guid, 1, 20, params+",法院层级:最高法院")
+			// ids, err := ListContent(client, number, guid, 1, 20, params+",法院层级:最高法院")
 
-			//log.Println("list-content", params, len(ids), err)
+			// log.Println("list-content", params, len(ids), err)
 
 			for _, instance := range items["审判程序"] {
 				params := params + ",审判程序:" + instance.key
@@ -116,7 +115,7 @@ func mainxii() {
 				for _, high := range items["法院地域"] {
 					params := params + ",法院层级:高级法院,法院地域:" + high.key
 					causeExpand(items, params)
-					//	ids, err = ListContent(client, number, guid, 1, 20, params)
+					// 	ids, err = ListContent(client, number, guid, 1, 20, params)
 				}
 				for _, intermediate := range items["中级法院"] {
 					params := params + ",法院层级:中级法院,法院地域:" + intermediate.key
@@ -201,32 +200,6 @@ func treeItemConvert(item map[string]interface{}, ret map[string][]Parameter) {
 	for _, child := range children {
 		treeItemConvert(child.(map[string]interface{}), ret)
 	}
-}
-
-// SubmitForm ...
-func SubmitForm(client *tools.Client, uri string, body url.Values) (*http.Response, error) {
-	req, _ := http.NewRequest("POST", uri, bytes.NewBufferString(body.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-
-	resp, err := client.Do(req)
-	verb("POST", uri, resp, err)
-	return resp, err
-}
-
-type opt = func(url.Values)
-
-func set(k, v string) opt {
-	return func(body url.Values) {
-		body.Set(k, v)
-	}
-}
-func Submit(client *tools.Client, uri string, params ...opt) (*http.Response, error) {
-	body := url.Values{}
-	for _, set := range params {
-		set(body)
-	}
-	return SubmitForm(client, uri, body)
 }
 
 func treeExpand(client *tools.Client,
@@ -333,33 +306,12 @@ func TreeList(client *tools.Client) (ret string, err error) {
 	return JSONStringBody(resp.Body)
 }
 
-// JSONStringBody ...
-func JSONStringBody(r io.Reader) (ret string, err error) {
-	body, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-	vm := otto.New()
-	ret, err = vmRunS(vm, string(body))
-	if err != nil {
-		return
-	}
-	ret, err = vmRunS(vm, fmt.Sprintf(`JSON.stringify(%s)`, ret)) // -> json string
-	return
-}
-
-// VL5X ...
-func VL5X(client *tools.Client) string {
-	vjkl5 := GetVJKL5FromCookie(client)
-	ret, _ := vl5x(vjkl5)
-	return ret
-}
 func CaseDetail(raw string) (summary, dir, detail map[string]interface{}) {
 	r1 := regexp.MustCompile(`stringify\((\{.*?\})\);`)
 	tmp := r1.FindStringSubmatch(raw)[1] // tmp is javascript json object, but we treat it as a json string
 	r1 = regexp.MustCompile(`\\/Date\((\d+)\)`)
 	tmp = r1.ReplaceAllString(tmp, `$1`)
-	//var ccase map[string]interface{}
+	// var ccase map[string]interface{}
 	_ = json.Unmarshal([]byte(tmp), &summary)
 
 	vm := otto.New()
@@ -371,22 +323,10 @@ func CaseDetail(raw string) (summary, dir, detail map[string]interface{}) {
 	r1 = regexp.MustCompile(`jsonHtmlData\s?=\s?("\{.*\}");`)
 	tmp = r1.FindStringSubmatch(raw)[1]
 	tmp, _ = vmRunS(vm, tmp)
-	//fmt.Println(prettys(tmp))
+	// fmt.Println(prettys(tmp))
 	_ = json.Unmarshal([]byte(tmp), &detail)
 
 	return
-}
-
-// GetVJKL5FromCookie ...
-func GetVJKL5FromCookie(client *tools.Client) string {
-	uri, _ := url.Parse(host)
-	cookies := client.Jar.Cookies(uri)
-	for _, ck := range cookies {
-		if ck.Name == "vjkl5" {
-			return ck.Value
-		}
-	}
-	return ""
 }
 
 // Criminal ...
@@ -409,63 +349,10 @@ func Home(client *tools.Client) {
 	}
 }
 
-func prettys(js string) string {
-	var ret bytes.Buffer
-	_ = json.Indent(&ret, []byte(js), "", "  ")
-	return ret.String()
-}
-
-func pretty(jo interface{}) string {
-	b, _ := json.MarshalIndent(jo, "", "  ")
-	return string(b)
-}
-
 // CaseContent ...
 // http://wenshu.court.gov.cn/content/content?DocID=d8952be5-e5a2-4b8b-b554-cccf5824617f&KeyWord=%E5%86
 func CaseContent(docid string) {
 
-}
-
-// AESKey ...
-// https://www.jianshu.com/p/1dc99e3d927c
-func AESKey(runeval string) (key string, err error) {
-	vm := otto.New()
-	compile(vm, config.js, "docid.js")
-	js, err := vm.Run(fmt.Sprintf(`GetJs("%v")`, runeval))
-	if err != nil {
-		return
-	}
-	jss, err := js.ToString()
-	if err != nil {
-		return
-	}
-	statements := strings.Split(jss, ";;")
-	statements[0] = statements[0] + ";" // $hidescript=...
-	js, err = vm.Run(statements[0])     // Tm('._KEY="6942871305;,*Mh)
-	if err != nil {
-		return
-	}
-	jss, err = js.ToString()
-	if err != nil {
-		return
-	}
-	log.Println(jss)
-
-	r := regexp.MustCompile(`_\[_\]\[_\]\((.*?)\)\(\);`)
-	xs := r.FindStringSubmatch(statements[1])[1]
-	xs = strings.Replace(xs, "$hidescript", strconv.Quote(jss), -1)
-	js, err = vm.Run(xs)
-	if err != nil {
-		return
-	}
-	jss, err = js.ToString()
-	if err != nil {
-		return
-	}
-	// setTimeout('com.str._KEY="a69e42871c4f499c930c755edbf6d7d1";',8000*Math.random());
-	r = regexp.MustCompile(`_KEY="(.*)";'`)
-	key = r.FindStringSubmatch(jss)[1]
-	return
 }
 
 const host = "http://wenshu.court.gov.cn"
@@ -483,36 +370,6 @@ var (
 	CheckCodeURL         = host + "/Content/CheckVisitCode"
 	VisitRemindURL       = host + "/Html_Pages/VisitRemind.html"
 )
-
-// GetCode ...
-func GetCode(client *tools.Client, guid string) (number string) {
-	data := url.Values{}
-	data.Set("guid", guid)
-
-	req, _ := http.NewRequest("POST", GetCodeURL, bytes.NewBufferString(data.Encode()))
-	req.Header.Set("Origin", host)
-	req.Header.Set("Referer", host)
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-	b, _ := ioutil.ReadAll(resp.Body)
-
-	number = string(b)
-	return
-}
-
-// GUID ...
-func GUID() string {
-	uuid := make([]byte, 16)
-	rand.Read(uuid)
-
-	return fmt.Sprintf("%x-%x-%x%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
-}
 
 // CaseSummary ...
 type CaseSummary struct {
@@ -616,32 +473,6 @@ func ListContent(client *tools.Client, number, guid string,
 	return
 }
 
-func vmRunS(vm *otto.Otto, src string) (string, error) {
-	val, err := vm.Run(src)
-	if err == nil {
-		return val.ToString()
-	}
-	return "", err
-}
-
-func vl5x(vjkl5 string) (string, error) {
-	vm := otto.New()
-	compile(vm, path.Join(config.js, "vl5x.js"))
-
-	value, err := vm.Run(fmt.Sprintf(`GetVl5x("%v")`, vjkl5))
-	if err != nil {
-		return "", err
-	}
-	return value.ToString()
-}
-func compile(vm *otto.Otto, files ...string) {
-	for _, file := range files {
-		if bjs, err := ioutil.ReadFile(file); err == nil {
-			vm.Run(string(bjs))
-		}
-	}
-}
-
 // ValidateCode ...
 func ValidateCode(client *tools.Client) (err error) {
 	// req, _ := http.NewRequest("POST", ValidateCodeURL, )
@@ -669,6 +500,15 @@ func ValidateCode(client *tools.Client) (err error) {
 	if err != nil {
 		return
 	}
+	text = strings.Map(func(r rune) rune {
+		if r == 'B' {
+			return '8'
+		}
+		if r == 'O' {
+			return '0'
+		}
+		return r
+	}, text)
 	code := url.Values{
 		"ValidateCode": []string{text},
 	}
